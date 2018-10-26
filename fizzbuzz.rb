618d2231afa9b43a492369097db21145a5ea6983
Dr. XyClo <sdcarmody@gmail.com>
2018-10-26 01:12:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
+x = gets.strip
 def fizzbuzz(x)
   if x % 3 == 0
+    
     x = fizz_3
   end
 end
